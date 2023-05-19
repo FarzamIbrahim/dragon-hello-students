eac90f11318219c8de2d72fe0d305ab1ae47b5f0
Ibrahim Farzam <ibra9419@gmail.com>
2023-05-18 22:41:19 -0400

    today commit

---
+++ src/com/hello/AnotherClass.java
@@ -0,0 +1,7 @@
+package com.hello;
+
+public class AnotherClass {
+public static void main(String[] args) {
+	System.out.println("My file added");
+}
+}
---
+++ src/com/hello/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.hello;
+
+public class HelloWorld {
+public static void main(String[] args) {
+	System.out.println("Hello world this time from GIT and Git Hup");
+}
+}
